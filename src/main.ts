b50b2484554cc489d6a59292f8fadfb76df267ed
qy <qy@MBP-2CN684MD6M-44.local>
2023-09-26 16:01:53 +0800

    fix: 更新接口版本号

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,13 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { HttpLoggerMiddleware } from './middleware/http-logger.middleware';
 import { ErrorFilter } from './common/error-filter';
+import { VersioningType } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableVersioning({
+    type: VersioningType.URI,
+  });
   app.useGlobalFilters(new ErrorFilter());
   app.use(new HttpLoggerMiddleware().use);
   await app.listen(3000);
